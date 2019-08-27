facf76425b0ffe3d2c8ebfe70b70d4d663062805
LXW-PC\LXW <932049492@qq.com>
2019-08-27 21:41:33 +0800

    使用了webpack和vue-cli的vue项目

---
+++ demo2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!--使用组件标签-->
+    <vuedemo/>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import vuedemo from './components/vuedemo.vue'
+// 映射组件标签
+export default{
+  components: {
+    vuedemo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ demo2/src/components/vuedemo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+// 配置组件
+export default {
+  data () {
+    return {
+      msg: 'Hello'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
